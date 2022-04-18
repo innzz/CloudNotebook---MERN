41b1b24003dc3ec0ef9ede82a82945ac78cfe1bb
inzz <inzamampawaskar10@gmail.com>
2022-04-19 04:09:47 +0530

    Added various functions to note like add,update,delete and made a working api for frontend

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -14,7 +14,8 @@ router.get('/getnotes',fetchuser, async (req,res)=>{
 //ROUTE 2: POST add a note using "/api/notes/addnote"
 router.post('/addnote',fetchuser,[
     body('title','Enter atleast 4 letters').isLength({min:4}),
-    body('description','Enter atleast 4 letters').isLength({min:4})
+    body('description','Enter atleast 4 letters').isLength({min:4}),
+    body('tag','Enter atleast 4 letters').isLength({min:4})
 ],async (req,res)=>{
     try {
         const {title,description,tag} = req.body;
@@ -26,16 +27,73 @@ router.post('/addnote',fetchuser,[
         };
 
         const notes = new Note({
-            title,description,tag,user:req.user
+            title,description,tag, user: req.user
         });
 
         const savedNote = await notes.save();
+
         res.json(savedNote);
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Internal error occured');
     }
+
 })
 
+//ROUTE 3: PUT update a note using "/api/notes/updatenote/:id"
+router.put('/updatenote/:id',fetchuser,[
+    body('title','Enter atleast 4 letters').isLength({min:4}),
+    body('description','Enter atleast 4 letters').isLength({min:4}),
+    body('tag','Enter atleast 4 letters').isLength({min:4})
+],async (req,res)=>{
+    try {
+        const {title,description,tag} = req.body;
+
+        // If there are errors, return Bad requests and errors 
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        };
+        const newNote = {};
+        if(title){newNote.title = title};
+        if(description){newNote.description = description};
+        if(tag){newNote.tag = tag};
+
+
+        let note = await Note.findById(req.params.id);
+        if(!note){res.status(404).send("Not found")};
+
+        if(note.user.toString() !== req.user){
+            res.status(404).send("Not allowed");
+        };
+
+        note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+        res.json({note});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Internal error occured');
+    }
+
+});
+
+    //ROUTE 4: DELETE delete a note using "/api/notes/deletenote/:id"
+    router.delete('/updatenote/:id',fetchuser,async (req,res)=>{
+        try {
+
+            let note = await Note.findById(req.params.id);
+            if(!note){res.status(404).send("Not found")};
+
+            if(note.user.toString() !== req.user){
+                res.status(404).send("Not allowed");
+            };
+
+            note = await Note.findByIdAndDelete(req.params.id);
+            res.json({"seccess":"note has been deleted",note:note});
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send('Internal error occured');
+        }
+
+    });
 
 module.exports = router
